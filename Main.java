a5effc21cda11ff8c19eb856fe08c059c0376766
Liam Goodier <liamgoodier1@gmail.com>
2025-04-15 17:09:53 -0300

    asking the user to input his name and then greeting user

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args){
         System.out.println("Hello World");
+  
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter your name mate: ");
+        String name = input.nextLine();
+
+        System.out.println("Hello " + name + "!");
+        input.close();
+
+
+
+  
     }
+    
 }
