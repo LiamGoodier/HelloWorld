4e781186512b09705ca5796f062c58d4f176e2cb
Liam Goodier <liamgoodier1@gmail.com>
2025-04-15 17:20:20 -0300

    requested the user's name and age, classified their age group afterwards

--- Main.java
+++ Main.java
@@ -2,16 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args){
-        System.out.println("Hello World");
-  
+
         Scanner input = new Scanner(System.in);
         System.out.print("enter your name mate: ");
         String name = input.nextLine();
 
         System.out.println("Hello " + name + "!");
+
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        if(age < 12){
+            System.out.println("You are a kid.");
+        }else if(age <18){
+            System.out.println("You are a teenager");
+            
+        }else{
+            System.out.println("Youre an adult or a very old man/woman");
+        }
+
         input.close();
-
-
 
   
     }
